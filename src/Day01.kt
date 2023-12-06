e02286b83b7ae7c58e5b6c692108a310ca06da50
JuliaSchling <127942352+juliaschling@users.noreply.github.com>
2023-12-06 13:28:04 +0100

    added my code for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,84 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    var total = 0
+    input.forEach { line ->
+
+      var firstDigit: Char? = null
+      var secondDigit: Char? = null
+
+      line.toCharArray()
+        .forEach { char ->
+          if (char in '0'..'9') {
+            if (firstDigit == null) {
+              firstDigit = char
+            } else {
+              secondDigit = char
+            }
+          }
+        }
+
+      if (firstDigit == null) {
+        firstDigit = '0'
+      }
+      if (secondDigit == null) {
+        secondDigit = firstDigit
+      }
+
+      total += "$firstDigit$secondDigit".toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    return total
+  }
+
+  fun part2(input: List<String>): Int {
+    var total = 0
+    input.forEach { line ->
+
+      var firstDigit: Char? = null
+      var secondDigit: Char? = null
+
+      line.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+        .replace("zero", "z0o")
+        .toCharArray()
+        .forEach { char ->
+          if (char in '0'..'9') {
+            if (firstDigit == null) {
+              firstDigit = char
+            } else {
+              secondDigit = char
+            }
+          }
+        }
+
+      if (firstDigit == null) {
+        firstDigit = '0'
+      }
+      if (secondDigit == null) {
+        secondDigit = firstDigit
+      }
+
+      total += "$firstDigit$secondDigit".toInt()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    return total
+  }
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  // test if implementation meets criteria from the description, like:
+  val testInput1 = readInput("1Dec_example_part1")
+  check(part1(testInput1) == 142)
+
+  val testInput2 = readInput("1Dec_example_part2")
+  check(part2(testInput2) == 281)
+
+  val input = readInput("1Dec_own")
+  part1(input).println()
+  part2(input).println()
 }
